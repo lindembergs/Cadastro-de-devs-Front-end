b07a9614bd0123dbe3551a25c4653efb9375ffe3
Lindemberg <lindembergparaiba@gmail.com>
2024-10-24 17:40:18 -0300

    fix: conexao dos labels com os inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
 export function App() {
   return (
-    <div className="max-w-custom-1000 mx-auto ">
+    <div className="max-w-custom-1000 mx-auto">
       <h1 className="text-zinc-50 text-4xl my-6">Programadores</h1>
+      <form className="flex flex-col">
+        <label className="text-zinc-50 text-lg" htmlFor="name">
+          Nome:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-3"
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Digite seu nome"
+        />
+        <label className="text-zinc-50 text-lg" htmlFor="photo">
+          Foto:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-3"
+          id="photo"
+          name="photo"
+          type="text"
+          placeholder="Coloque seu avatar"
+        />
+        <label className="text-zinc-50 text-lg" htmlFor="position">
+          Ocupação:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-3"
+          id="position"
+          name="position"
+          type="text"
+          placeholder="Digite sua profissão"
+        />
+        <label className="text-zinc-50 text-lg" htmlFor="linkedin">
+          LinkedIn:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-3"
+          id="linkedin"
+          name="linkedin"
+          type="text"
+          placeholder="Coloque seu LinkedIn"
+        />
+      </form>
     </div>
   );
 }
