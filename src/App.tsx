a97bd83328aa9ac922c22f6e02a37f3eaf8ff297
Lindemberg <lindembergparaiba@gmail.com>
2024-11-04 19:30:22 -0300

    Fix: form field

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,60 @@ export function App() {
     <div className="max-w-custom-1000 mx-auto">
       <h1 className="text-zinc-50 text-4xl my-6">Programadores</h1>
       <form className="flex flex-col" onSubmit={handleCreateCustomers}>
-        {/* Campos do formulário */}
+        <label className="text-zinc-50 text-lg cursor-pointer" htmlFor="name">
+          Nome:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-8"
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Digite seu nome"
+          ref={nameRef}
+          required
+        />
+        <label className="text-zinc-50 text-lg cursor-pointer" htmlFor="photo">
+          Foto:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-8"
+          id="photo"
+          name="photo"
+          type="text"
+          placeholder="Coloque seu avatar"
+          ref={imageRef}
+          required
+        />
+        <label
+          className="text-zinc-50 text-lg cursor-pointer"
+          htmlFor="position"
+        >
+          Ocupação:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-8"
+          id="position"
+          name="position"
+          type="text"
+          placeholder="Digite sua profissão"
+          ref={positionRef}
+          required
+        />
+        <label
+          className="text-zinc-50 text-lg cursor-pointer"
+          htmlFor="linkedin"
+        >
+          LinkedIn:
+        </label>
+        <input
+          className="h-10 rounded pl-3 w-full mb-8"
+          id="linkedin"
+          name="linkedin"
+          type="text"
+          placeholder="Coloque seu LinkedIn"
+          ref={linkedinRef}
+          required
+        />
         <button className="w-full h-10 rounded bg-cyan-600" type="submit">
           Cadastrar
         </button>
