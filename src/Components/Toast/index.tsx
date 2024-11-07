f1fe21be78ec357a6b7a1238793e5cd3e8305bc3
Lindemberg <lindembergparaiba@gmail.com>
2024-11-07 19:11:13 -0300

    feat: toast works with prop

--- src/Components/Toast/index.tsx
+++ src/Components/Toast/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-export function Toast() {
-  const notify = () => toast("Usuário cadastrado com sucesso!");
+interface ToastProps {
+  message: string;
+}
+export function Toast({ message }: ToastProps) {
+  const notify = () => toast(`${message}`);
   useEffect(() => {
     notify();
   }, []);
